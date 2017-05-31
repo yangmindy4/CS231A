839f252748dd4dac19c792fc4a31eeda72818ff7
Tommy Fan <lecheng.fan@stanford.edu>
2017-05-31 13:05:31 -0700

    experiment with projecting onto a plane perpendicular to optical axis

--- milestone.py
+++ milestone.py
@@ -64,20 +64,41 @@ def transform_image(H, image, indices):
         new_image[indices[i][1], indices[i][0]] = image[int(corresponding_points[i][1]), int(corresponding_points[i][0])]
     return new_image
 
-def main():
-    folded_envelope = mpimg.imread("images/Envelope2.jpg")
-    plt.imshow(folded_envelope)
-    #flat portion corners
-    flat_corners = np.array([
-        [360.481, 597.411],
-        [1519.57, 646.386],
-        [1462.43, 1670.79],
-        [99.277, 1605.49]])
+
+def dog_example():
+    dog_image = mpimg.imread("images/Dog2.jpg")
+    plt.imshow(dog_image)
+    plt.show()
+    #Dog1.jpg flat portion corners
+    # flat_corners = np.array([
+    #     [378.78, 685.616],
+    #     [1394.08, 770.14],
+    #     [1364.68, 1527.18],
+    #     [89.466, 1450.01]])
+    # folded_corners = np.array([
+    #     [1394.08, 770.14],
+    #     [2371.62, 303.419],
+    #     [2588.77, 1042.09],
+    #     [1364.68, 1527.18]
+    #     ])
+
+    #Dog2.jpg flat portion corners
+    # flat_corners = np.array([
+    #     [428.145, 657.36],
+    #     [1460.76, 777.252],
+    #     [1398.88, 1554.62],
+    #     [126.481, 1423.12]])
+    ground_truth = np.array([
+        [0, 0],
+        [1200, 0],
+        [1200, 1050],
+        [0, 1050]
+        ])
     folded_corners = np.array([
-        [1519.57, 646.386],
-        [2756.2, 262.744],
-        [2964.35, 1499.38],
-        [1462.43, 1670.79]
+        [1460.76, 777.252],
+        [2284.54, 57.8991],
+        [2385.09, 815.927],
+        [1398.88, 1554.62],
         ])
     top_right_projected = 2*flat_corners[1]-flat_corners[0]
     bottom_right_projected = 2*flat_corners[2]-flat_corners[3]
@@ -91,15 +112,52 @@ def main():
     reprojection = calculate_projection(H, folded_corners)
 
     folded = mppath.Path(np.r_[reprojection, reprojection[0][None,:]], closed=True)
-    xx, yy = np.meshgrid(np.arange(0, folded_envelope.shape[1]), np.arange(0, folded_envelope.shape[0]))
+    xx, yy = np.meshgrid(np.arange(0, dog_image.shape[1]), np.arange(0, dog_image.shape[0]))
     xx = np.reshape(xx, -1)
     yy = np.reshape(yy, -1)
     zipped = zip(xx, yy)
     flattened_portion = np.array(zipped)[np.where(folded.contains_points(zipped) == True)[0]]
-    new_image = transform_image(H, folded_envelope, flattened_portion)
+    new_image = transform_image(H, dog_image, flattened_portion)
     plt.imshow(new_image)
     plt.show()
+
+def main():
+    # folded_envelope = mpimg.imread("images/Envelope2.jpg")
+    # plt.imshow(folded_envelope)
+    # #flat portion corners
+    # flat_corners = np.array([
+    #     [360.481, 597.411],
+    #     [1519.57, 646.386],
+    #     [1462.43, 1670.79],
+    #     [99.277, 1605.49]])
+    # folded_corners = np.array([
+    #     [1519.57, 646.386],
+    #     [2756.2, 262.744],
+    #     [2964.35, 1499.38],
+    #     [1462.43, 1670.79]
+    #     ])
+    # top_right_projected = 2*flat_corners[1]-flat_corners[0]
+    # bottom_right_projected = 2*flat_corners[2]-flat_corners[3]
+    # # plt.plot(top_right_projected[0], top_right_projected[1], 'bo')
+    # # plt.plot(bottom_right_projected[0], bottom_right_projected[1], 'bo')
+
+    # projection = np.r_[flat_corners[1][None,:], top_right_projected[None,:], \
+    #     bottom_right_projected[None,:],flat_corners[2][None,:]]
+    # H = compute_homography(folded_corners, projection)
+   
+    # reprojection = calculate_projection(H, folded_corners)
+
+    # folded = mppath.Path(np.r_[reprojection, reprojection[0][None,:]], closed=True)
+    # xx, yy = np.meshgrid(np.arange(0, folded_envelope.shape[1]), np.arange(0, folded_envelope.shape[0]))
+    # xx = np.reshape(xx, -1)
+    # yy = np.reshape(yy, -1)
+    # zipped = zip(xx, yy)
+    # flattened_portion = np.array(zipped)[np.where(folded.contains_points(zipped) == True)[0]]
+    # new_image = transform_image(H, folded_envelope, flattened_portion)
+    # plt.imshow(new_image)
+    # plt.show()
     # triangle_example(H)
+    dog_example()
    
 
 if __name__ == '__main__':
